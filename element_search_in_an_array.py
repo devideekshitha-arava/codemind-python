103e516a974a53b3e32acb1776af552d3c6260ca
devideekshitha-arava <84397428+devideekshitha-arava@users.noreply.github.com>
2022-09-12 15:23:56 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] a=int(input()) print(a in l)

--- element_search_in_an_array.py
+++ element_search_in_an_array.py
@@ -1,9 +1,4 @@
 n=int(input())
-a=list(map(int,input().split()))
-k=int(input())
-for i in a:
-    if k==i:
-        print(True)
-        break
-else:
-    print(False)
+l=list(map(int,input().split()))[:n]
+a=int(input())
+print(a in l)
